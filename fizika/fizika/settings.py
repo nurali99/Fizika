f4ed72385477f3ec62c1e28a29193348282f2122
unknown <nurali07052020@gmail.com>
2022-03-29 00:01:41 +0500

    o'zgartirilgan holat

--- fizika/fizika/settings.py
+++ fizika/fizika/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#ro^4@=%l6&5v2zd^5wgu38!tdnk3m0h8*azt38#kx0d3br7iz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
